7bba373a5d37bd73c24fdf401f671a69d5a48d58
hababisha <haileamlakderbie1@gmail.com>
2025-07-19 15:29:33 +0300

    Initial commit and socket connection

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:3000')
+
+function App() {
+
+  return (
+    <>
+      <div>hello world</div>
+    </>
+  )
+}
+
+export default App
